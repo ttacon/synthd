2695b9a24a24dcbf9600bee3daf03d5b5ad898e7
Trey Tacon <ttacon@gmail.com>
2021-05-25 15:39:10 -0400

    feat: add ipAddress field support

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,6 +1,7 @@
 import Date from './date';
 import Email from './email';
 import FirstName from './firstName';
+import IPAddress from './ipAddress';
 import LastName from './lastName';
 import LinkedField from './linkedField';
 import MongoObjectID from './mongoObjectID';
@@ -11,6 +12,7 @@ export {
     Date,
     Email,
     FirstName,
+    IPAddress,
     LastName,
     LinkedField,
     MongoObjectID,
---
+++ src/fields/ipAddress.ts
@@ -0,0 +1,54 @@
+import {
+    SerializableField,
+    GeneratableField,
+    SynthdType
+} from '../';
+
+import * as faker from 'faker';
+
+type IPAddressOptions = {
+    ipv6: boolean,
+};
+
+class IPAddress implements GeneratableField {
+    fieldName: string;
+    options: IPAddressOptions;
+
+    constructor(fieldName: string,options: IPAddressOptions) {
+        this.fieldName = fieldName;
+        this.options = options;
+    }
+
+    name(): string {
+        return this.fieldName;
+    }
+
+    type(): SynthdType {
+        return {
+            typeName(): string {
+                return 'IPAddress';
+            }
+        };
+    }
+
+    generate(): SerializableField {
+        const fieldName = this.fieldName;
+        const typeFunc = this.type;
+        const options = this.options;
+        return {
+            name(): string {
+                return fieldName;
+            },
+            type: typeFunc,
+            value(): any {
+                if (options.ipv6) {
+                    return faker.internet.ipv6();
+                }
+                return faker.internet.ip();
+            },
+        }
+    }
+
+}
+
+export default IPAddress;
